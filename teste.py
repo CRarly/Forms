7e09418de53a0d926c9c168576ef5590f8f0a7a8
CRarly <93451949+CRarly@users.noreply.github.com>
2024-05-31 16:16:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ from sqlalchemy import create_engine, inspect, text
 import re
 
 # Configurações de URLs e Token
-LOCAL_API_URL = 'https://api.seudominio.com/'  # URL base da sua API
-API_TOKEN = 'seu-token-de-acesso'  # Seu token de acesso estático
+LOCAL_API_URL = 'https://crarly.github.io/Forms/teste.py'  # URL base da sua API
+API_TOKEN = 'WDGmkgdVuRzYZU6VCHF0Og'  # Seu token de acesso estático
